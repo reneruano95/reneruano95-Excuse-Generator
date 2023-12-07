b8b2e2bef91fb624c47eaca343cdc1a52c589f62
Rene Ruano <105190254+reneruano95@users.noreply.github.com>
2023-12-07 00:35:32 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,38 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let random1 = Math.floor(Math.random() * 4);
+  let random2 = Math.floor(Math.random() * 3);
+  let random3 = Math.floor(Math.random() * 5);
+
+  console.log(
+    who[random1] +
+      " " +
+      action[random1] +
+      " " +
+      what[random2] +
+      " " +
+      when[random3]
+  );
+  document.getElementById("excuseGenerator").innerHTML =
+    who[random1] +
+    " " +
+    action[random1] +
+    " " +
+    what[random2] +
+    " " +
+    when[random3];
   //write your code here
   console.log("Hello Rigo from the console!");
 };
